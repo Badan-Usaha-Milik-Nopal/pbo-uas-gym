4e12c011a094a295d000df1dba83e61704174a9a
s0nchae <noval.dwiputra@gmail.com>
2025-11-30 21:46:13 +0700

    Nambahin akun admin

---
+++ akunAdmin.cs
@@ -0,0 +1,13 @@
+// class admin
+
+class AdminAccount
+{
+    public string username;
+    public string password;
+
+    public AdminAccount(string uname, string pw)
+    {
+        username = uname;
+        password = pw;
+    }
+}
